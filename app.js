e4f09c50c619c2c9b32c3be3c8d363b860849c87
mzosam <88102418+mzosam@users.noreply.github.com>
2026-02-06 11:25:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,29 +13,3 @@ function checkPin(){
     else{
         alert("Page can't be found");
     }
-
-    const targetDate = new Date("2026-03-01T00:00:00").getTime();
-    const day = document.getElementById("day");
-    const hour = document.getElementById("hour");
-    const minute = document.getElementById("minute");
-    const second = document.getElementById("second");
-    
-    const countdown = setInterval(() => {
-    const now = new Date().getTime();
-    const distance = targetDate - now;
-    
-    if (distance < 0) {
-    clearInterval(countdown);
-    return;
-    }
-    
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    day.innerHTML = days;
-    hour.innerHTML = hours;
-    minute.innerHTML = minutes;
-    second.innerHTML = seconds;
-    }, 1000);
